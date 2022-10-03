9e22cff46812ac2ade1d8055bd3b5071e99e7aaa
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-10-03 13:36:37 +0300

    task 1 rdy

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const delayInterval = 1000;
+let timerId = 0
 
+const refs = {
+    body: document.body,
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn:document.querySelector('[data-stop]')
+}
+
+refs.startBtn.addEventListener('click', onStartClick)
+refs.stopBtn.addEventListener('click', onStopClick)
+
+function onStartClick() {
+    refs.startBtn.disabled = true
+    refs.stopBtn.disabled = false
+   timerId =  setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, delayInterval);
+}
+
+
+function onStopClick() {
+    refs.startBtn.disabled = false
+    refs.stopBtn.disabled = true
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
